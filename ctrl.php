6a7f4ad2fed1656140d93f71d8977c1c3986f82d
GandzioreQ <danielgandyra@gmail.com>
2021-03-25 20:26:28 +0100

    New structure and extension

---
+++ ctrl.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'init.php';
+// Skrypt kontrolera głównego uruchamiający określoną
+// akcję użytkownika na podstawie przekazanego parametru
+
+//każdy punkt wejścia aplikacji (skrypt uruchamiany bezpośrednio przez klienta) musi dołączać konfigurację
+// - w tym wypadku jest już tylko jeden punkt wejścia do aplikacji - skrypt ctrl.php
+
+switch ($action) {
+	default : // 'calcView'
+	    // załaduj definicję kontrolera
+		include_once 'app/controllers/CalcCtrl.class.php';
+		// utwórz obiekt i uzyj
+		$ctrl = new CalcCtrl ();
+		$ctrl->generateView ();
+	break;
+	case 'calcCompute' :
+		// załaduj definicję kontrolera
+		include_once 'app/controllers/CalcCtrl.class.php';
+		// utwórz obiekt i uzyj
+		$ctrl = new CalcCtrl ();
+		$ctrl->process ();
+	break;
+	case 'action1' :
+		// zrób coś innego ...
+		print('hello');
+	break;
+	case 'action2' :
+		// zrób coś innego ...
+		print('goodbye');
+	break;
+}
